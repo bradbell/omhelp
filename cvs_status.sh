c7d1da8495119a228cd10b64028e3b40c4087fbb
Brad <>
2009-02-09 02:07:18 +0000

    ignore more status entries

--- cvs_status.sh
+++ cvs_status.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cvs update | sed \
 	-e '/^[?] wish_list.htm$/d'                 \
-	-e '/^[?].*\.bin$/d'                      \
+	-e '/^[?].*\.bin$/d'                        \
 	-e '/^[?].*\.cache$/d'                      \
 	-e '/^[?].*\.gz$/d'                         \
 	-e '/^[?].*\.ilk$/d'                        \
@@ -11,16 +11,18 @@ cvs update | sed \
 	-e '/^[?].*\.suo$/d'                        \
 	-e '/^[?].*\.tmp$/d'                        \
 	-e '/^[?].*\.zip$/d'                        \
-	-e '/^[?].*[/ ]\.deps$/d'                     \
-	-e '/^[?].*[/ ]config\./d'                   \
-	-e '/^[?].*[/ ]configure$/d'                   \
-	-e '/^[?].*[/ ]Debug$/d'                      \
-	-e '/^[?].*[/ ]Doc$/d'                        \
+	-e '/^[?].*[/ ]\.deps$/d'                   \
+	-e '/^[?].*[/ ]confdefs\./d'                \
+	-e '/^[?].*[/ ]conftest\./d'                \
+	-e '/^[?].*[/ ]config\./d'                  \
+	-e '/^[?].*[/ ]configure$/d'                \
+	-e '/^[?].*[/ ]Debug$/d'                    \
+	-e '/^[?].*[/ ]Doc$/d'                      \
 	-e '/^[?].*[/ ]junk$/d'                     \
 	-e '/^[?].*[/ ][Mm]akefile$/d'              \
 	-e '/^[?].*[/ ][Mm]akefile.in$/d'           \
 	-e '/^[?].*[/ ]omhelp-[0-9-]*$/d'           \
-	-e '/^[?].*[/ ]stamp-h1$/d'                   \
+	-e '/^[?].*[/ ]stamp-h1$/d'                 \
 	-e '/^[?] src\/latex.c$/d'                  \
 	-e '/^[?] src\/latex.h$/d'                  \
 	-e '/^[?] src\/lex.tex.c$/d'                \
