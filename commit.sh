866922713f75c9e448938c3e1d0b0087a36b6adf
bradbell <>
2010-10-23 14:32:04 +0000

    This is a template file for making commits to the OMhelpPackage repository. Lines with no 'at' characters, are general comments not connected to a specifi file. Lines containing an 'at' character are "file name" followed by comment; for example
    commit.sh: For this example, commit.sh is the only file committed.

---
+++ commit.sh
@@ -0,0 +1,79 @@
+# ! /bin/bash
+# ---------------------------------------------------------------------------
+# OMhelp: Source Code -> Help Files: Copyright (C) 1998-2008 Bradley M. Bell
+# License: GNU General Public License; version 2 or higher.
+# ---------------------------------------------------------------------------
+#
+# replacement text for this commit
+cat << EOF > commit.$$
+This is a template file for making commits to the OMhelpPackage repository.
+Lines with no 'at' characters, are general comments not connected to 
+a specifi file. Lines containing an 'at' character are "file name" 
+followed by comment; for example
+
+commit.sh@ For this example, commit.sh is the only file committed.
+EOF
+# -----------------------------------------------------------------------
+if [ "$1" == 'files' ]
+then
+	cvs update 2> /dev/null | sed -n \
+		-e '/^M /p' \
+			-e 's/^M //' \
+			-e 's/$/@/' | sort -u
+	rm commit.$$
+	exit 0
+fi
+if [ "$1" != '' ]
+then
+cat << EOF
+usage: ./commit.sh files
+       ./commit.sh
+
+The first from lists the files that have changed and should be
+listed at the beginning of the file commit.sh.
+
+The second form actually commits the files (provided that you reply
+y to the [y/n] prompt that commit.sh generates).
+EOF
+	rm commit.$$
+	exit 0
+fi
+# -----------------------------------------------------------------------
+dir=`pwd | sed -e 's|.*/OMhelpPackage/\([^/]*\).*|\1|'`
+list=`sed -e '/@/! d' -e 's/@.*//' commit.$$`
+msg=`sed -e '/@ *$/d' -e 's|.*/\([^/]*@\)|\1|' -e 's|@|:|' commit.$$`
+# -----------------------------------------------------------------------
+for file in $list
+do
+	echo "sed -f commit.sed < $file > commit.$$"
+	sed -f commit.sed < $file > commit.$$
+	diff $file commit.$$
+done
+rm commit.$$
+echo "--------------------------------------------------------------------"
+echo "cvs commit -m \""
+echo "$msg"
+echo "\" \\"
+echo "$list"
+read -p "is this ok [y/n] ?" response
+if [ "$response" != "y" ]
+then
+     exit 1
+fi
+for file in $list
+do
+	echo "sed -f commit.sed -i $file"
+done
+#
+echo "cvs commit -m '$msg' $list"
+if ! cvs commit -m "$msg" $list
+then
+	echo "commit.sh: failed"
+	exit 1
+fi
+#
+echo "mv commit.sh commit.old"
+mv commit.sh commit.old
+#
+echo "cvs update commit.sh"
+cvs update commit.sh
