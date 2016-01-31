473ff84ffbc86f07ad52b9b80636897fc328573a
Brad Bell <bradbell@seanet.com>
2016-01-30 17:53:18 -0700

    highlight.sh: Example extern "C" routine that does in memory conversion.

--- bug/highlight.sh
+++ bug/highlight.sh
@@ -25,53 +25,91 @@ cat << EOF > hilite.cpp
 # include <iostream>
 # include <fstream>
 # include <sstream>
+# include <cstdlib>
 # include "srchilite/sourcehighlight.h"
 # include "srchilite/langmap.h"
 
 # define DATADIR "/home/bradbell/prefix/highlight/share/source-highlight"
+
+extern "C" char* highlight(
+	const char* input_text  ,
+	const char* input_lang  ,
+	const char* output_lang )
+{	using std::string;
+
+	// input string stream
+	std::stringstream input_sstream( input_text );
+
+	// ctor
+	srchilite::SourceHighlight hiliter( output_lang );
+
+	// source-highlight data directory
+	hiliter.setDataDir(DATADIR);
+
+	// output stream
+	std::stringstream output_sstream;
+
+	// convert from the input to output string stream
+	hiliter.highlight(input_sstream, output_sstream, string(input_lang) );
+
+	// return value
+	size_t len = output_sstream.str().size();
+	char* ret = static_cast<char*>( std::malloc( (len + 1) * sizeof(char) ) );
+	for(size_t i = 0; i < len; i++)
+		ret[i] = output_sstream.str()[i];
+	ret[len] = '\0';
+
+	return ret;
+}
 
 int main(int argc, char *argv[])
 {	using std::string;
 
 	// get input and output file names
 	if( argc != 3 )
-	{	std::cerr << "usage: highlight input_file output_file" << std::endl;
+	{	std::cerr << "usage: hilite input_file output_file" << std::endl;
 		return 1;
 	}
 	const char* input_file  = argv[1];
 	const char* output_file = argv[2];
 
 	// input file language
-	string input_lang = "cpp.lang";
+	srchilite::LangMap lang_map(DATADIR, "lang.map");
+	string input_lang = lang_map.getMappedFileNameFromFileName(input_file);
 
 	// output file language
 	string output_lang = "html.outlang";
 
-	// ctor
-	srchilite::SourceHighlight hiliter(output_lang);
-
-	// source-highlight data directory
-	hiliter.setDataDir(DATADIR);
-
 	// input stream
 	std::fstream input_stream(input_file, std::fstream::in);
 
-	// input string stream
-	std::stringstream input_sstream;
+	// read file into a string
+	string input_string;
 	char ch;
 	while( input_stream.get(ch) )
-		input_sstream << ch;
-
-	// output stream
-	std::stringstream output_sstream;
-
-	// convert from the input to output string stream
-	hiliter.highlight(input_sstream, output_sstream, input_lang);
+		input_string += ch;
 
 	// output stream
 	std::fstream output_stream(output_file, std::fstream::out);
-	while( output_sstream.get(ch) )
-		output_stream << ch;
+
+	if( input_lang != "" )
+	{	char* output_text = highlight(
+			input_string.c_str(),
+			input_lang.c_str(),
+			output_lang.c_str()
+		);
+
+		// output highlighted test
+		output_stream << output_text;
+
+		// free memory allocated by highlight
+		std::free(output_text);
+	}
+	else
+	{	// output text as input
+		output_stream << input_string.c_str();
+	}
+
 
 	return 0;
 }
