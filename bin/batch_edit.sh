29b27d7d3b18d0c4f6b5b63898b9ece01053669b
Brad Bell <bradbell@seanet.com>
2016-02-02 19:07:56 -0700

    Change src_example -> srcfile_example.

--- bin/batch_edit.sh
+++ bin/batch_edit.sh
@@ -6,31 +6,13 @@
 #             GNU General Public License Version 2.
 # -----------------------------------------------------------------------------
 move_list='
-	omh/src.omh
-	omh/no_src.omh
-	omh/getstarted/src_example.omh
-	omh/getstarted/src_example.py
-	omh/getstarted/start_src.omh
 '
 move_sed='s|src|srcfile|'
 #
 no_change_list='
-	src/relative.c
 '
 cat << EOF > junk.sed
-s|src_example\.|srcfile_example.|
-s|src_omh|srcfile_omh|
-s|src\.omh|srcfile.omh|
-s|^\\tsrc\$|&\\n\\tsrcfile|
-#
-s|\$src\\([^a-z_]\)|\$srcfile\\1|g
-s|\$cref src\\([^a-z_]\)|\$cref srcfile\\1|g
-s|\$head src\\([^a-z_]\)|\$head srcfile\\1|g
-s|\$begin src\\([^a-z_]\)|\$begin srcfile\\1|g
-s|\$mref/src/|\$mref/srcfile/|g
-s|@src\\([^a-z_]\)|@srcfile\\1|g
-#
-s|\\tsrc py newline|\\tsrcfile py newline|
+s|src_example|srcfile_example|g
 EOF
 # -----------------------------------------------------------------------------
 if [ $0 != "bin/batch_edit.sh" ]
