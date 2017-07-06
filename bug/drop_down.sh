98de8ee2be1b7e5b97480c0b39a1da331b66ec5a
Brad Bell <bradbell@seanet.com>
2017-07-06 07:25:55 -0700

    bug in drop down menus

---
+++ bug/drop_down.sh
@@ -0,0 +1,66 @@
+#! /bin/bash -e
+# -----------------------------------------------------------------------------
+# OMhelp: Language Independent Embedded Documentation
+#           Copyright (C) 1998-2015 Bradley M. Bell
+# OMhelp is distributed under the terms of the
+#             GNU General Public License Version 2.
+# -----------------------------------------------
+# bash function that echos and executes a command
+echo_eval() {
+	echo $*
+	eval $*
+}
+# -----------------------------------------------
+#
+if [ -e htm ]
+then
+	echo_eval rm -r htm
+fi
+echo_eval mkdir htm
+echo "create htm/one.omh"
+cat << EOF > htm/one.omh
+@begin one@@
+@spell
+	xml
+	Firefox
+	Linux
+	htm
+@@
+@section One@@
+
+@head Problem@@
+The drop down menus in the file @href%one.xml%@@
+(e.g, @code one->@@)
+do not work for 32 bit Firefox 54.0.1 on Windows.
+They do work for this file using 64 bit Firefox 52.0 on Linux.
+They also work for the file @href%one.htm%@@ using
+32 bit Firefox 54.0.1 32 on Windows.
+
+
+@children%two.omh%@@
+
+@end
+EOF
+echo "create htm/two.omh"
+cat << EOF > htm/two.omh
+@begin two@@
+@section Two@@
+
+See @cref/problem/one/Problem/@@.
+
+@end
+EOF
+sed -e 's|@|$|g' -i htm/one.omh
+sed -e 's|@|$|g' -i htm/two.omh
+#
+program='../build/src/omhelp'
+if [ ! -e "$program"  ]
+then
+	echo "$program does not exist"
+	echo 'The following commands will build it:"'
+	echo '	cd .. ; bin/run_cmake ; cd build ; make'
+	exit 1
+fi
+echo_eval cd htm
+echo_eval ../$program ./one.omh -noframe -omhelp_dir ../../omhelp_data
+echo_eval ../$program ./one.omh -noframe -xml -omhelp_dir ../../omhelp_data
